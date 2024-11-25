8dc2e28714e9a43dd742d3ef441653e420ce47c7
Simon Danielsson <sd223ah.student.lnu.se>
2024-11-25 13:37:49 +0100

    Initial commit

---
+++ app/src/main/java/main/App.java
@@ -0,0 +1,15 @@
+package main;
+
+/**
+ * The starting point of the application.
+ */
+public class App {
+  /**
+   * The starting point of the application.
+   *
+   * @param args - !Not used!
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
