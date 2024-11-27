5651312da49d9311a6c72e153cf03b18afc94eef
Simon Danielsson <sd223ah.student.lnu.se>
2024-11-27 14:26:45 +0100

    RED : should be able to change line color of shape

--- app/src/test/java/uml/shapes/ShapeTests.java
+++ app/src/test/java/uml/shapes/ShapeTests.java
@@ -23,4 +23,15 @@ public class ShapeTests {
     Assertions.assertEquals(actual.getGreen(), expected.getGreen());
     Assertions.assertEquals(actual.getBlue(), expected.getBlue());
   }
+
+  @Test
+  public void shouldBeAbleToChangeLineColor() {
+    Color expected = new Color(255, 255, 255);
+    sut.changeLineColor(expected);
+
+    Color actual = sut.getLineColor();
+    Assertions.assertEquals(actual.getRed(), expected.getRed());
+    Assertions.assertEquals(actual.getGreen(), expected.getGreen());
+    Assertions.assertEquals(actual.getBlue(), expected.getBlue());
+  }
 }
