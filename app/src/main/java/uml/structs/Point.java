657859dc6c7a319aa7c47e84f8a68a50bcc37d4e
Simon Danielsson <sd223ah.student.lnu.se>
2024-11-28 10:22:32 +0100

    REFACTOR : Create point struct

---
+++ app/src/main/java/uml/structs/Point.java
@@ -0,0 +1,28 @@
+package uml.structs;
+
+/**
+ * A structure for a point in 2D space.
+ */
+public class Point {
+  public float x;
+  public float y;
+
+  /**
+   * Constructs a new point and sets x and y to 0.
+   */
+  public Point() {
+    x = 0;
+    y = 0;
+  }
+
+  /**
+   * Constructs a point and assigns given x and y value
+   *
+   * @param x - The x value.
+   * @param y - The y value.
+   */
+  public Point(float x, float y) {
+     this.x = x;
+     this.y = y;
+  }
+}
