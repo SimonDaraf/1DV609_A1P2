007128165f1b4a6a3d36638db2d37dfa8835cb5e
Simon Danielsson <sd223ah.student.lnu.se>
2024-12-02 14:45:05 +0100

    Add in house cloneable interface

---
+++ app/src/main/java/util/Cloneable.java
@@ -0,0 +1,15 @@
+package util;
+
+/**
+ * Provides an in-house cloneable interface.
+ * @param <T> - The cloneable type.
+ */
+public interface Cloneable<T> {
+  /**
+   * Returns a new instance with identical members.
+   * !May not be a deep copy!
+   *
+   * @return - The cloned instance.
+   */
+  public T clone();
+}
